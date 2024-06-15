5128a091082e95c11565ca76270ef858d437af92
JackDS <manojit.dhar.jack@gmail.com>
2024-06-15 12:29:47 +0530

    changes to getname.php

--- admin/get-name.php
+++ admin/get-name.php
@@ -25,7 +25,7 @@ if(!empty($_POST["rid"])) {
     while($row=$stmt->fetch(PDO::FETCH_ASSOC))
     {
     ?>
-    <?php echo htmlentities($row['fees']); ?>
+    <?php echo htmlentities($row['firstName']); ?>
     <?php
     }
 }
