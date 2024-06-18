ab5ef76d06af3bd1944b2f99ed1f3a3b5f19327d
Alam-Monir <monirrza0641@gmail.com>
2024-06-18 21:47:54 +0530

    fix

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -133,7 +133,7 @@
                                     <div class="d-inline-flex align-items-center">
                                         <h2 class="text-dark mb-1 font-weight-medium"><?php include 'counters/booked-count.php'?></h2>
                                     </div>
-                                    <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Booked Rooms</h6>
+                                    <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Hostel Students</h6>
                                 </div>
                                 <div class="ml-auto mt-md-3 mt-lg-0">
                                     <span class="opacity-7 text-muted"><i data-feather="book-open"></i></span>
